b6cbebec13c4ef406156ed0b7b019b768c50ad9d
Natalia <nata6duma@gmail.com>
2025-11-30 19:29:01 +0200

    changes

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,9 +4,9 @@ const Body = () => {
   return (
     <main style={styles.body}>
       <div style={styles.card}>
-        <img src={"/avatar-default.png"} alt="John Doe" style={styles.photo} />
+        <img src={"/photo.jpg"} alt="Natalia Dumanska" style={styles.photo} />
         <div style={styles.textContainer}>
-          <h2 style={styles.name}>John Doe</h2>
+          <h2 style={styles.name}>Natalia Dumanska</h2>
           <p style={styles.position}>Software engineer</p>
         </div>
       </div>
